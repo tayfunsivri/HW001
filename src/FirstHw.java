ae47ef08b79eea5ef9cc45ceb0182b7fd7644b44
tayfunsivri <tsivri@gmail.com>
2022-09-22 23:19:36 +0300

    hw1 bitti.

--- src/FirstHw.java
+++ src/FirstHw.java
@@ -2,5 +2,6 @@ public class FirstHw {
     public static void main(String[] args) {
         //TODO: Lütfen ekrana Merhaba Dünya! yazan bir java programı yazınız.
         System.out.println("Merhaba Dünya");
+        System.out.println("Hello World");
     }
 }
